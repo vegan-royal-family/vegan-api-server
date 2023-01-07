ff7753189755ce727a1dcebe7a966f889a8b48e6
seonkyo <tjs0466@naver.com>
2023-01-08 03:58:25 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import { UserModule } from './user/user.module';
         port: Number(process.env.DB_PORT),
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        database: process.env.DB_DATABASE,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         migrations: [__dirname + '/migration/*.{ts,js}'],
         migrationsRun: true,
