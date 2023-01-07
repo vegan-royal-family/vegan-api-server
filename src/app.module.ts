d16d544859cb737d9b0532b37c5bdbc2df73553e
seonkyo <tjs0466@naver.com>
2023-01-08 04:38:30 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,7 +32,7 @@ import { UserModule } from './user/user.module';
           type: 'mysql',
           host: process.env.DB_HOST,
           port: Number(process.env.DB_PORT),
-          username: process.env.DB_USER,
+          username: process.env.DB_USERNAME,
           password: process.env.DB_PASSWORD,
           database: process.env.DB_DATABASE,
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
