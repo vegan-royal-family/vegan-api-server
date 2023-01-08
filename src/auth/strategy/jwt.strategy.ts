02140fe8b5228961f11824d8c47e7730ce8742ab
seonkyo <tjs0466@naver.com>
2023-01-08 23:52:05 +0900

    readinessProbe, startupProbe 정의

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: config.get('jwt.secret') ?? String(process.env.JWT_SECRET),
+      secretOrKey: config.get('jwt.secret') ?? process.env.JWT_SECRET,
     });
   }
 
