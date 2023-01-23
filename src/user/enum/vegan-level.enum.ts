b31c4b1e02067ffe1efdd772a3f645fdb98307d8
seonkyo <tjs0466@naver.com>
2023-01-24 03:05:31 +0900

    modify vegan level

--- src/user/enum/vegan-level.enum.ts
+++ src/user/enum/vegan-level.enum.ts
@@ -2,7 +2,7 @@ import { registerEnumType } from '@nestjs/graphql';
 
 export enum VeganLevel {
   COMPLETELY = 'completely',
-  preferably = 'preferably',
+  PREFERABLY = 'preferably',
   INTEREST = 'interest',
 }
 registerEnumType(VeganLevel, { name: 'VeganLevel', description: 'VeganLevel' });
