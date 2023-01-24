b4d1754838c7df60e8007ba6ad1aa2a62815d51a
seonkyo <tjs0466@naver.com>
2023-01-25 00:40:30 +0900

    env 수정

--- config/default.js
+++ config/default.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: 3200,
   db: {
+    port: process.env.DB_PORT || 3306,
     host: process.env.DB_HOST || 'localhost',
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || 'root',
